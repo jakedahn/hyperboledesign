bdc902b64110c5ec9e693571fc4f8efa958ea88d
Jake Dahn <admin@jakedahn.com>
2011-02-21 01:16:24 -0600

    adding assets and pulling in haml

---
+++ public/js/applications.js
@@ -0,0 +1,19 @@
+jQuery.fx.interval = 1000;
+
+$.fn.pulsefade = function(interval) {
+  return this.each(function() {
+    $this = $(this);
+    $this.animate({ 'opacity': 'toggle' }, interval);
+    
+    setInterval(function(){
+      $this.animate({ 'opacity': 'toggle' }, interval);
+    }, interval)
+  });
+};
+
+$(function(){
+ // $("#top_layer").pulsefade(60000);
+ // $("#top_layer").pulsefade(30000);
+ // $("#top_layer").pulsefade(10000);
+ // $("#top_layer").pulsefade(5000);
+})
