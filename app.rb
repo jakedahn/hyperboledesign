4258a226860df17b1986dff0272b2e22e719ee21
Jake Dahn <admin@jakedahn.com>
2011-03-06 00:47:29 -0600

    set email to return false in json if error

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'sass'
 require 'haml'
 require 'pony'
+require 'json'
 
 module SinatraApp
   class App < Sinatra::Base
@@ -15,19 +16,27 @@ module SinatraApp
     
     post '/contact' do
  
-      Pony.mail(:to => "jake@hyperboledesign.com", 
+      begin
+        Pony.mail(:to => "jake@hyperboledesign.com", 
                 :from => params[:contact]["email"],
                 :subject => "Contact Form Inquiry from #{params[:contact]['name']}",
                 :body => erb(:email),
+                :via => :smtp,
                 :via_options => {
-                  :address         => "smtp.sendgrid.net",
+                  :address         => "smtp.sendgrid.com",
                   :port            => '25',
                   :user_name       => ENV['SENDGRID_USERNAME'],
                   :password        => ENV['SENDGRID_PASSWORD'],
                   :authentication  => :login,
                   :domain          => ENV['SENDGRID_DOMAIN']
-                })
-                
+                }
+        )
+        
+        return {:status => true}.to_json
+      rescue
+        return {:status => false}.to_json
+      end
+              
     end
 
   end
