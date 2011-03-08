ee48c80f47ec4476e4f48bc24401d35f8dfab47a
Jake Dahn <admin@jakedahn.com>
2011-03-07 18:41:10 -0600

    updating pony params

--- app.rb
+++ app.rb
@@ -30,12 +30,12 @@ module SinatraApp
                 :body => erb(:email),
                 :via => :smtp,
                 :via_options => {
-                  :address         => "smtp.sendgrid.com",
-                  :port            => '25',
-                  :user_name       => ENV['SENDGRID_USERNAME'],
-                  :password        => ENV['SENDGRID_PASSWORD'],
-                  :authentication  => :plain,
-                  :domain          => ENV['SENDGRID_DOMAIN']
+                  :address        => "smtp.sendgrid.net",
+                  :port           => "25",
+                  :authentication => :plain,
+                  :user_name      => ENV['SENDGRID_USERNAME'],
+                  :password       => ENV['SENDGRID_PASSWORD'],
+                  :domain         => ENV['SENDGRID_DOMAIN']
                 }
         )
         return {:success => true }.to_json
